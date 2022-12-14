3447844746c8ba487afb6794749810a1849ac74a
Balazs Zaicsek <zebalu@gmail.com>
2022-12-14 07:00:57 +0100

    Day14

--- aoc2022/src/main/java/io/github/zebalu/aoc2022/App.java
+++ aoc2022/src/main/java/io/github/zebalu/aoc2022/App.java
@@ -43,6 +43,7 @@ public class App {
         days.add(new DayData(11, "Monkey in the Middle", Day11::main));
         days.add(new DayData(12, "Hill Climbing Algorithm", Day12::main));
         days.add(new DayData(13, "Distress Signal", Day13::main));
+        days.add(new DayData(14, "Regolith Reservoir", Day14::main));
         Instant beforAll = Instant.now();
         for (var day : days) {
             System.out.println(day.header());
