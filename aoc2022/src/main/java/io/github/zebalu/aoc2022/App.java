272973ade068e07ff46b2a9911976ab150e7e0d7
Balazs Zaicsek <zebalu@gmail.com>
2022-12-25 06:48:04 +0100

    Day25

--- aoc2022/src/main/java/io/github/zebalu/aoc2022/App.java
+++ aoc2022/src/main/java/io/github/zebalu/aoc2022/App.java
@@ -54,6 +54,7 @@ public class App {
         days.add(new DayData(22, "Monkey Map", Day22::main));
         days.add(new DayData(23, "Unstable Diffusion", Day23::main));
         days.add(new DayData(24, "Blizzard Basin", Day24::main));
+        days.add(new DayData(25, "Full of Hot Air", Day25::main));
         Instant beforAll = Instant.now();
         for (var day : days) {
             System.out.println(day.header());
